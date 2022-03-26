8973022b08edcac17630c907fab3662adbd49902
marcoscarvalho-tk <marcoscarvalho.com@outlook.com>
2022-03-26 01:41:24 -0300

    insert new src/components/Expeneses.js and Expenses.css

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseDate(props){
     const month = props.date.toLocaleString('en-US',{month: 'long'});
     const day = props.date.toLocaleString('en-US',{day: '2-digit'});
     const year = props.date.getFullYear();
 
     return(
-      <div>
+      <div className="expenses-date">
         <div>{month}</div>
         <div>{day}</div>
         <div>{year}</div>
