38475c5c0be641359c7c245850357116d36d79b7
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 <marcoscarvalho.com@outlook.com>
2022-06-01 21:17:43 -0300

    update ExpenseForm.js improvement of event Handlers

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    // Esta é uma forma convencional de uso do Sate
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    // Esta é uma maneira de agrupar States, usando para isto um objeto
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: '',
+    // });
+
+    const titleChangeHandler = (event) =>{
+        setEnteredTitle(event.target.value);
+
+        // Este tipo de função não é recomendada caso 
+        // necessite manter o setState atualizado na próxima seção
+        // });
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // });
+
+        // Recomenda-se usar este tipo de sintaxe sempre que uma 
+        // atualização de estado depender do estado anterior.
+        // setUserInput((pervState) => {
+        //     return {...prevState, enteredTitle: event.target.value};
+        // });
+    };
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+
+        // Este tipo de função não é recomendada caso 
+        // necessite manter o setState atualizado na próxima seção
+        // });
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // });
+
+        // Recomenda-se usar este tipo de sintaxe sempre que uma 
+        // atualização de estado depender do estado anterior.
+        // setUserInput((pervState) => {
+        //     return {...prevState, enteredAmount: event.target.value};
+        // });
+    };
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+
+        // Este tipo de função não é recomendada caso 
+        // necessite manter o setState atualizado na próxima seção
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value,
+        // });
+
+        // Recomenda-se usar este tipo de sintaxe sempre que uma 
+        // atualização de estado depender do estado anterior.
+        // setUserInput((pervState) => {
+        //     return {...prevState, enteredDate: event.target.value};
+        // });
+    };
+const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date(enteredDate)
+    };
+    
+    console.log(expenseData);
+};
 return (
-    <form>
+    <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text"/>
+                <input type="text" onChange={titleChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" setp="0.01"/>
+                <input type="number" min="0.01" setp="0.01" onChange={amountChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31"/>
+                <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
             </div>
         </div>
         <div className="new-expense__actions">
