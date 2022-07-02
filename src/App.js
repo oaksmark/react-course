6b1e3d4cf8570675b32d40e285b25e7926820280
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 <marcoscarvalho.com@outlook.com>
2022-07-01 23:04:33 -0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import NewUserData from './components/UserData/NewUserData';
+import React, { useState } from "react";
+import NewUserData from "./components/UserData/NewUserData";
 
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
-    { text: 'Do all exercises!', id: 'g1' },
-    { text: 'Finish the course!', id: 'g2' }
+    { text: "Do all exercises!", id: "g1" },
+    { text: "Finish the course!", id: "g2" },
   ]);
 
-  const addNewUser = enteredText => {
-    setCourseGoals(prevGoals => {
+  const addNewUser = (enteredText) => {
+    setCourseGoals((prevGoals) => {
       const updatedGoals = [...prevGoals];
       updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
       return updatedGoals;
@@ -17,8 +17,15 @@ const App = () => {
 
   return (
     <div>
-    <NewUserData newUser={addNewUser}/>
-    <p>{courseGoals}</p>
+      <NewUserData newUser={addNewUser} />
+
+      <ul className="goal-list">
+        {courseGoals.map((goal) => (
+          <li key={goal.id} id={goal.id}>
+            {goal.text}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
