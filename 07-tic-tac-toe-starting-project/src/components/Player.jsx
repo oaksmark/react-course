db455fb01f9c5be1dcc3c4fad04712ae837b8b53
marcoscarvalho-tk <marcoscarvalho.com@outlook.com>
2024-04-05 01:06:40 -0300

    add new component Player.jsx file

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,25 @@
-export default function Player({...props}) {
+import { useState } from "react";
+
+export default function Player({ ...props }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(true);
+    console.log(isEditing);
+  }
+
+  let playerName = <span className="player-name">{props.name}</span> 
+
+  if(isEditing){
+     playerName =  <input type="text" className="player input" required></input>
+  }
   return (
-      <li>
-        <span className="player">
-          <span className="player-name">{props.name}</span>
-          <span className="player-symbol">{props.symbol}</span>
-        </span>
-        <button>Edit</button>
-      </li>
+    <li>
+      <span className="player">
+         {playerName}
+        <span className="player-symbol">{props.symbol}</span>
+        <button onClick={handleEditClick}>Save</button>
+      </span>
+    </li>
   );
 }
