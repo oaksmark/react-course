ac0f2c000448eaa584fb1bdc5c0f4c769e024d23
marcoscarvalho-tk <marcoscarvalho.com@outlook.com>
2024-04-07 01:17:24 -0300

    add new component Log.jsx file

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,24 +1,55 @@
-import { useState } from 'react';
-import Player from './components/Player.jsx';
-import GameBoard from './components/GameBoard.jsx';
+import { useState } from "react";
+import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
+import Log from "./components/Log.jsx";
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare(){
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+  // A função abaixo envia os ídices ordenados em um array para o state 
+  // via props ao component GameBoard  organizar os valores adicionados a matriz 
+  // Isso possibilita que um mesmo update atualize dois components simultaneamente
+  // Neste caso tanto o compontent Log quanto o GameBord recebem os mesmos valores.
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
     <main>
       <div id="game-container">
-        <ol id="players" className='highlight-player'>
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          turns={gameTurns}
+        />
       </div>
-      LOG
+      <Log />
     </main>
   );
 }
--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,22 +1,33 @@
-import { useState } from "react";
-
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
-export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
-    const[gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard;
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        setGameBoard((prevGameBoard) =>{
-            const updatedBorad = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBorad[rowIndex] [colIndex] = activePlayerSymbol;
-            return updatedBorad;
-        });
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
 
-        onSelectSquare();
-    }
+    gameBoard[row][col] = player;
+  }
+  //   A função abaixo foi substituida por outra localizada no component App.jsx
+  //   Este é um exemplo de "deriving state", o qual recebe via pros (no caso "turns")
+  //   os valores de indices ogranizados do update realizado simultaneamente nos components
+  //   Log.jsx e GameBoard.jsx.
+
+  // const[gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  // function handleSelectSquare(rowIndex, colIndex) {
+  //     setGameBoard((prevGameBoard) =>{
+  //         const updatedBorad = [...prevGameBoard.map(innerArray => [...innerArray])];
+  //         updatedBorad[rowIndex] [colIndex] = activePlayerSymbol;
+  //         return updatedBorad;
+  //     });
+
+  //     onSelectSquare();
+  // }
 
   return (
     <ol id="game-board">
@@ -25,7 +36,9 @@ export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
