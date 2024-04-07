c356f6ccb1ef697c515d53bedc3e9bf7b282a929
marcoscarvalho-tk <marcoscarvalho.com@outlook.com>
2024-04-06 23:03:15 -0300

    add new compontent GameBoard.jsx file

---
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
+    const[gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) =>{
+            const updatedBorad = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedBorad[rowIndex] [colIndex] = activePlayerSymbol;
+            return updatedBorad;
+        });
+
+        onSelectSquare();
+    }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,24 +1,49 @@
 import { useState } from "react";
 
-export default function Player({ ...props }) {
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
-    console.log(isEditing);
+    // setIsEditing(isEditing ? false : true);
+    // setIsEditing(!isEditing);
+    // A função abaixo tem o mesmo efeito das acima
+    // porém, garante o valor do último state 
+    // caso haja uma chamada dupla ou simultânea de setState 
+    // na mesma função.
+    setIsEditing((editing)=>!editing);
+  }
+  // Esta função insere o caracter digitado em um objeto
+  // em que: 
+  // event - é o gatilho que dispara o chamando a função => onChange{}
+  // target - é local onde está ocorrendo o event => <input/>
+  // value - é o valor inserido no input => texto, data, email...  
+  function handleChange(event){
+    setPlayerName(event.target.value);
+    console.log(event.target.value);
   }
 
-  let playerName = <span className="player-name">{props.name}</span> 
+  // Esta função apenas limpa o campo do input no momento em que o 
+  // cursor seleciona o local (onFocus) de digitação
+  // function handleClear(){
+  //   isEditing && setPlayerName('');
+
+  // }
+    
+
+  let editablePlayerName = <span className="player-name">{playerName}</span> 
+  let btnCaption = 'Edit';
 
   if(isEditing){
-     playerName =  <input type="text" className="player input" required></input>
+    editablePlayerName =  <input type="text" required value={playerName} onChange={handleChange}/>
+     btnCaption = 'Save';
   }
   return (
-    <li>
+    <li className={isActive ? 'active' : undefined}>
       <span className="player">
-         {playerName}
-        <span className="player-symbol">{props.symbol}</span>
-        <button onClick={handleEditClick}>Save</button>
+         {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+        <button onClick={handleEditClick}>{btnCaption}</button>
       </span>
     </li>
   );
